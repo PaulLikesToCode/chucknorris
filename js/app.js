41bf12ed65cfe1f638e893c4c5c039b28ae44115
paullikestocode <paul_schrage@yahoo.com>
2014-10-01 18:32:59 -0700

    More functionality: save data to model, get quotes

--- js/app.js
+++ js/app.js
@@ -2,16 +2,23 @@ var app = angular.module('chucknorris', []);
 
 app.factory('quotes', [function() {
 	var q = {
-		quotes: ["Chuck Norris Quote", "Another Chuck Norris Quote"]
+		quotes: []
 	};
 	return q;
-}])
+}]);
 
+app.controller('getRandomFact', ['$scope', '$http', 'quotes', function($scope, $http, quotes) {
 
+	$scope.getChuckQuote = function() {
+		$http.get('http://api.icndb.com/jokes/random').
+			success(function(data, status) {
+				$scope.quote = data.value.joke;
+			}).error(function(data,status) {
+				$scope.quote = "Sorry, Chuck Norris is busy right now";
+			});
+	}
 
-app.controller('getRandomFact', ['$scope', 'quotes', function($scope, $http) {
-	$scope.quotes = quotes.quotes
-	$scope.getQuote = function() {
+	$scope.getCustomQuote = function() {
 		$http.get('http://api.icndb.com/jokes/random?firstName='+$scope.person.first_name+'&lastName='+$scope.person.last_name).
 			success(function(data, status) {
 				$scope.quote = data.value.joke;
@@ -20,9 +27,12 @@ app.controller('getRandomFact', ['$scope', 'quotes', function($scope, $http) {
 			});
 		}
 
-	// $scope.quotes = quotes.quotes;
-	// $scope.saveQuote = function() {
-	// 	$scope.quotes.push($scope.quote);
-	// }
-}]);	
+	$scope.saveQuote = function() {
+		quotes.quotes.push($scope.quote);
+	}
+}]);
 
+app.controller('showQuotes', ['$scope', 'quotes', function($scope, quotes) {
+	$scope.quotes = quotes.quotes;
+
+}]);
