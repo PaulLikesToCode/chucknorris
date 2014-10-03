33d1913aaba08bb6e72ca43aed0aa83179b9749c
paullikestocode <paul_schrage@yahoo.com>
2014-10-02 17:30:27 -0700

    Most of the front end is there

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-var app = angular.module('chucknorris', []);
+var app = angular.module('chucknorris', ['ngRoute', 'ui.bootstrap']);
+
 
 app.factory('quotes', [function() {
 	var q = {
@@ -7,12 +8,25 @@ app.factory('quotes', [function() {
 	return q;
 }]);
 
+app.controller('facebook', ['$scope', function($scope) {
+	$scope.getFriends = function() {
+		FB.api(
+    		"/me/friends",
+    		function (response) {
+      		if (response && !response.error) {
+        		console.log(response);
+      	}
+    }
+);
+	}
+}]);
+
 app.controller('getRandomFact', ['$scope', '$http', 'quotes', function($scope, $http, quotes) {
 
 	$scope.getChuckQuote = function() {
 		$http.get('http://api.icndb.com/jokes/random').
 			success(function(data, status) {
-				$scope.quote = data.value.joke;
+				$scope.quote = (data.value.joke).replace(/&quot;/g, '"');
 			}).error(function(data,status) {
 				$scope.quote = "Sorry, Chuck Norris is busy right now";
 			});
@@ -21,7 +35,7 @@ app.controller('getRandomFact', ['$scope', '$http', 'quotes', function($scope, $
 	$scope.getCustomQuote = function() {
 		$http.get('http://api.icndb.com/jokes/random?firstName='+$scope.person.first_name+'&lastName='+$scope.person.last_name).
 			success(function(data, status) {
-				$scope.quote = data.value.joke;
+				$scope.quote = (data.value.joke).replace(/&quot;/g, '"');
 			}).error(function(data,status) {
 				$scope.quote = 'Sorry, Chuck Norris is busy right now.';
 			});
@@ -34,7 +48,7 @@ app.controller('getRandomFact', ['$scope', '$http', 'quotes', function($scope, $
 
 app.controller('showQuotes', ['$scope', 'quotes', function($scope, quotes) {
 	$scope.quotes = quotes.quotes;
-
+	$scope.dynamicTooltip = 'remove';
 	$scope.removeQuote = function(index) {
 		$scope.quotes.splice(index, 1);	
 	}
